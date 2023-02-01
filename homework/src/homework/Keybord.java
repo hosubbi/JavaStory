29d979275aadcee0f9d0896f02f0feae0bc2239a
곽호섭 <@LAPTOP-AFE8C4TA>
2023-02-01 23:29:11 +0900

    homework4

---
+++ homework/src/homework/Keybord.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class Keybord {
+	//필드
+	String keybord;
+	int price;
+	
+	//생성자
+	public Keybord() {
+		
+	}
+	public Keybord(String keybord,int price) {
+		
+	}
+	
+	//메소드
+
+	
+	
+}
---
+++ homework/src/homework/day04homework.java
@@ -0,0 +1,11 @@
+package homework;
+
+import java.util.Scanner;
+
+public class day04homework {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
