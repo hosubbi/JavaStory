749854060991a9f6964b801ce74cf4d9af79e697
admin <admin@YD05-18>
2023-02-03 17:51:58 +0900

    SD

---
+++ Chapter07/src/com/yedam/inheritance/Calculator.java
@@ -0,0 +1,9 @@
+package com.yedam.inheritance;
+
+public class Calculator {
+	//부모 클래스에서 원 넓이를 구하는 메소드
+	double areaCircle(double r) {
+		System.out.println("Calculator 객체의 메소드 실행");
+		return 3.14*r*r;
+	}
+}
---
+++ Chapter07/src/com/yedam/inheritance/Member.java
@@ -0,0 +1,9 @@
+package com.yedam.inheritance;
+
+public class Member {
+	//메소드
+	public final void memberInfo() {
+		System.out.println("회원 정보 출력");
+	}
+	
+}
