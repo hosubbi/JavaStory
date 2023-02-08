ed225ca2f02976096ff0e5690491407863438e29
admin <admin@YD05-18>
2023-02-08 17:46:01 +0900

    0208 17:45

--- 20230208/src/com/yedam/test/Q04Test.java
+++ 20230208/src/com/yedam/test/Q04Test.java
@@ -5,7 +5,9 @@ public class Q04Test {
 		String[] arr = {"010102-4", "991012-1", "960304-1", "881012-2", "040403-3"};
 		int man = 0;
 		int woman = 0;
+		//향상된 for문 써보자 (1)
 		for(int i=0; i<arr.length; i++) {
+			//equals써보자 (2) 
 			if(arr[i].charAt(7) == '1' || arr[i].charAt(7) == '3') {
 				man++;
 			}else{
