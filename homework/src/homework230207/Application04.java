fd57be25fcd2831121a64c99b65c86b01d62b5a7
admin <admin@YD05-18>
2023-02-08 09:42:28 +0900

    0208 09:42

--- homework/src/homework230207/Application04.java
+++ homework/src/homework230207/Application04.java
@@ -6,16 +6,25 @@ public class Application04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("입력 : ");
-		String input = sc.nextLine();
+		String strList = sc.nextLine();
+		StringBuilder sb = new StringBuilder();
+		//indexOf, charAt
+		//aaabbccceedddd
+		//1)charAt(0)~charAt(문자열.length()-1)
+		//2)charAt(0) - > a
+		//3)indexOf(a) -> a의 위치 -> index : 0
+		//4)indexOf(charAt(i)) == i
+		
 		System.out.print("출력 : ");
-//		System.out.print(input.charAt(3));
-		for (int i = 0; i<input.length()-1; i++) {
-			for (int j = i+1; j<input.length()-1; j++) {
-				if(input.charAt(i) == input.charAt(j)) {
-					System.out.print(input.charAt(i));
-					break;
-				}
+		for(int i=0; i<strList.length(); i++) {
+			if(strList.indexOf(strList.charAt(i)) == i) {
+//				System.out.println(i);
+				sb.append(strList.charAt(i));
 			}
 		}
+		System.out.println(sb);
+		
+	
+	
 	}
 }
