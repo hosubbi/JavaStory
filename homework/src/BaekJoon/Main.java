65cff9af99a7f886c2197ff827649bd4021917dc
곽호섭 <@LAPTOP-AFE8C4TA>
2023-03-06 00:28:07 +0900

    zz

--- homework/src/BaekJoon/Main.java
+++ homework/src/BaekJoon/Main.java
@@ -8,8 +8,27 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int x = sc.nextInt();
-		
+		int c = sc.nextInt();
+		int test[] = new int[c];
+		int a;
+		for(int i=0; i<c; i++) {
+			int n = sc.nextInt();
+			int count = 0;
+			int arr[] = new int[n];
+			for(int j=0; j<n; j++) {
+				a = sc.nextInt();
+				arr[j] = a;
+				test[i] += a;
+			}
+			test[i] = test[i] / n;
+			for(int z=0; z<n; z++) {
+				
+				if(arr[z] > test[i]) {
+					count += 1;
+				}
+				
+			}
+			System.out.printf("%.3f%%\n",(count/(double) n)*100);
+		}	
 	}
 }
-
