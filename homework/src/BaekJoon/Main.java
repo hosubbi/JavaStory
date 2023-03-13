11ca857c201acd26d45a1bc59eef57d512a7327d
곽호섭 <@LAPTOP-AFE8C4TA>
2023-03-13 23:25:12 +0900

    zzz

--- homework/src/BaekJoon/Main.java
+++ homework/src/BaekJoon/Main.java
@@ -6,26 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
+
 		
-		String num = sc.next();
-		int alpabet[] = new int[26];
-		for(int i=1; i<alpabet.length; i++) {
-			alpabet[0] = 99;
-			alpabet[i] = alpabet[i-1]+1;
-		}
 		
-		for(int i=0; i<num.length(); i++) {
-			for(int j=0; j<alpabet.length; j++) {
-				if(alpabet[j] == (num.charAt(i)-'0'+48)) {
-					alpabet[j] = i;
-				}
-				
-			}
-			
-		}
-		for (int obj : alpabet) {
-			System.out.println(obj);
-		}
+		
 	}
 }
